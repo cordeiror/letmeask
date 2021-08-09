d6c884b0421fc0eeb74c388ea201567498936695
cordeiror <renato.cordeiro@gmail.com>
2021-08-09 16:43:03 -0300

    add authcontext

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 //import {Button} from './components/Button';
 //import {FelsLike} from './components/FelsLike'
+import { createContext, useState, useEffect } from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
+import { AuthContextProvider } from './contexts/AuthContext';
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
+import { auth, firebase } from './services/firebase';
+
+type User = {
+  id: String;
+  name: String;
+  avatar: String;
+}
+
+type AuthContextType = {
+  user: User | undefined;
+  signInWithGoogle: () => Promise<void>;
+}
+
+export const AuthContext = createContext({} as AuthContextType);
 
 function App() {
+
+ 
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/rooms/new" component={NewRoom} />
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
